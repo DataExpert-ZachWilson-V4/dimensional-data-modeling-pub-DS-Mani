2afd934d0ed524b8ebb20758b7d1f28fa3bafb36
Manikanta Reddy Yerolla <161749075+DS-Mani@users.noreply.github.com>
2024-05-20 18:49:21 -0400

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,17 +1,31 @@
--- Define a custom type for film information
-CREATE TYPE film_struct AS (
-    film TEXT,             
-    votes INTEGER,         
-    rating FLOAT,          
-    film_id UUID,          
-    quality_class TEXT     
-);
-
--- Create the main actors table
-CREATE TABLE actors (
-    actor_id UUID PRIMARY KEY,  
-    actor TEXT NOT NULL,       
-    films film_struct[],        
-    is_active BOOLEAN,         
-    current_year INTEGER       
-);
+--  year for which we want to populate the actors table
+WITH current_year_data AS (
+    SELECT
+        actor_id,
+        actor_name AS actor,
+        ARRAY_AGG(ROW(film_name, votes, rating, film_id, 
+            CASE
+                WHEN rating > 8 THEN 'star'
+                WHEN rating > 7 THEN 'good'
+                WHEN rating > 6 THEN 'average'
+                ELSE 'bad'
+            END
+        )::film_struct) AS films,
+        MAX(release_year) AS current_year,
+        BOOL_OR(release_year = 2023) AS is_active
+    FROM
+        actor_films
+    WHERE
+        release_year = 2023
+    GROUP BY
+        actor_id, actor_name
+)
+INSERT INTO actors (actor_id, actor, films, is_active, current_year)
+SELECT
+    actor_id,
+    actor,
+    films,
+    is_active,
+    current_year
+FROM
+    current_year_data;
